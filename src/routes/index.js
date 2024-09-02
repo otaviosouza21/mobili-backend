8046aad7881f25baa7122bd46957e7befdb674c5
otaviosouza21 <otaviosouzalu@gmail.com>
2024-09-02 17:01:35 -0300

    ajuste de typescript

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const pedidos = require('./pedidos.js')
-const produtos_sku = require('./produtos-sku.js')
+const markup = require('./markup.js')
 const bodyParser = require('body-parser');
-import { Express } from "express";
 
-module.exports = (app:Express) =>{
+
+module.exports = (app) =>{
     app.use(cors());
     app.use(express.json())
     app.use(bodyParser.json());
     app.use(pedidos)
-    app.use(produtos_sku)
+    app.use(markup)
 }
 
 
